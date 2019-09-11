fdedfe51501d52309c241ba1b201bc9dfc055976
patriciaSR <patricia.suarez.psr@gmail.com>
2019-09-11 09:32:46 +0200

    add scss files to PokemonList and Pokemon components

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Pokemon from './Pokemon';
 import PropTypes from 'prop-types';
+import './PokemonList.scss';
 
 class PokemonList extends React.Component {
   render() {
