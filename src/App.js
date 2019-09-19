5bbbc30d98e01162daff2ecb822a67c1b84d1ce1
patriciaSR <patricia.suarez.psr@gmail.com>
2019-09-19 21:05:57 +0200

    improve exercise with filters and details pages

--- src/App.js
+++ src/App.js
@@ -45,3 +45,11 @@ render() {
 }
 
 export default App;
+
+//FASE 3 de MEJORA
+
+// En esta url https://raw.githubusercontent.com/Adalab/m3-evaluacion-intermedia-oneeyedman/componentbranch/pokemons.json podéis hacer la petición de los pokemons para el ejercicio de repaso de mañana.
+// Recordad que vamos a :
+// - pintar el listado de pokemons
+// - Filtrar por nombre de pokemon
+// - Crear una página principal, y una de detalle para cada criatura
